d6c66311a42ee94210a471b1b7df09039a0b1728
kukcat <galaxycraft3103@gmail.com>
2021-12-28 19:56:05 +0200

    finish lesson2

--- Lesson2/script.js
+++ Lesson2/script.js
@@ -2,6 +2,7 @@ let inputList = document.querySelector(".list_text");
 let todoList = document.querySelector(".todo");
 let todoListRestore = document.querySelector(".restore");
 let tagList = document.querySelector('.select');
+let tagListFind = document.querySelector('.select_find');
 
 let removeBtn = document.querySelector(".list_remove");
 let listAddBtn = document.querySelector(".list_add");
@@ -20,19 +21,25 @@ if(localStorage.getItem("todoArrRestore") != null) {
 
 }
 
-
+tagListFind.addEventListener('change', () => reCreate())
 removeBtn.addEventListener('click', () => deleteAll());
 listAddBtn.addEventListener('click', () => creator(inputList.value, tagList.value, getCurrentDate()));
 
     //-----------  Добавить элементы в массив и локальное хранилище  -------------
 
 let creator = (text, tag, date) => {
+
+    if(text == ''){
+        return;
+    }
+
     const task = {
         'text': text,
         'tag': tag,
         'date': date
     }
 
+    inputList.value = '';
     todoListArr.push(task); 
         //-----------  localStorage  -------------
 
@@ -45,6 +52,7 @@ let creator = (text, tag, date) => {
 let deleteAll = () =>  {
     localStorage.removeItem('todoArr');
     todoListArr = [];
+    todoListArrRestore = [];
     reCreate();
 }
 
@@ -65,50 +73,59 @@ let addTodoItem = (id, variant) =>{
         let newTodoItem = document.createElement('li');
         newTodoItem.classList.add('todo_item');
         newTodoItem.setAttribute('data-index-number', id);
-
+    
         if(variant == 1){
             todoList.append(newTodoItem);
             newTodoItem.innerHTML= ` 
-                <div class="todo_number">${id+1}</div>
-                <div class="todo_text" onclick="changeTodoText(this)">${todoListArr[id].text}</div>
+                <div class="todo_number">${num}</div>
+                <div class="todo_item_wrapper"><div class="todo_text" onclick="changeTodoText(this)">${todoListArr[id].text}</div>
                 <div class="todo_tag">${todoListArr[id].tag}</div>
                 <div class="todo_date">${todoListArr[id].date}</div>
-                <div class="todo_remove" onclick="removeTodoElement(this)">x</div>
+                <div class="todo_remove" onclick="removeTodoElement(this)">Удалить</div></div>
             `
+            num++;
+
         }else if(variant == 2){
             todoListRestore.append(newTodoItem);
                     newTodoItem.innerHTML= ` 
-                <div class="todo_number">${id+1}</div>
+                <div class="todo_number">${numRestore}</div>
+                <div class="todo_item_wrapper">
                 <div class="todo_text" onclick="changeTodoText(this)">${todoListArrRestore[id].text}</div>
                 <div class="todo_tag">${todoListArrRestore[id].tag}</div>
                 <div class="todo_date">${todoListArrRestore[id].date}</div>
-                <div class="todo_restore" onclick="restoreTodoElement(this)">Restore</div>
-                <div class="todo_remove" onclick="removeTodoRestoreElement(this)">x</div>
+                <div class="todo_restore" onclick="restoreTodoElement(this)">Восстановить</div>
+                <div class="todo_full_remove" onclick="removeTodoRestoreElement(this)">Удалить</div></div>
             `
+            numRestore++;
         }
-
         
 }
 
     //-----------  Пересоздать страницу  -------------
 
-let reCreate = () => {
+let reCreate = (parametr=tagListFind.value) => {
+
+    num = 1;
+    numRestore = 1;
     todoList.innerHTML = ``;
     todoListRestore.innerHTML = ``;
+    
     if(todoListArr != null){
         for (let i = 0; i < todoListArr.length; i++) {
-            addTodoItem(i,1);
-            
+            if(parametr=='Всё' || parametr == todoListArr[i].tag){
+                addTodoItem(i,1);
+                console.log(todoListArr[i].tag)
+                console.log(parametr)
+            }
         }
     }
     if(todoListArrRestore != null){
         for (let i = 0; i < todoListArrRestore.length; i++) {
-            addTodoItem(i,2);   
-            
+            if(parametr=='Всё' || parametr == todoListArrRestore[i].tag){
+                addTodoItem(i,2);
+            }
         }
     }
-    
-    
 }
 
 let restoreTodoElement = (element) => {
@@ -162,14 +179,9 @@ let changeTodoText = (element) =>{
 }
 
     //-----------  Закрыть окошко изменения элемента без изменений  -------------
-
-
 let removeChangeElement = (element) => {
     element.closest(".background").remove();
 }
-
-
-
 
 let changeTodoItem = (element) =>{
 
